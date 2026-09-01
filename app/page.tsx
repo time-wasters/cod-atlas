c271c525d2d3c8a5fffaf045f9dc878ef603a81d
Philipp Gächter <github@plpgtr.de>
2026-09-01 21:10:44 +0200

    Added "Prettier"

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { AtlasPageClient } from "@/src/infrastructure/framework/next/atlas-page.client";
 
+/**
+ * Application entry point.
+ *
+ * @returns AtlasPageClient
+ */
 export default function Page() {
   return <AtlasPageClient />;
 }
