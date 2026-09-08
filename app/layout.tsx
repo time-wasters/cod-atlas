aa1f3efbf04285aa3a87c53f7e874cc051133a5a
Philipp Gächter <github@plpgtr.de>
2026-09-08 23:48:39 +0200

    Fixed image references in build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@fontsource/noto-sans/latin-500.css";
+import "leaflet/dist/leaflet.css";
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
